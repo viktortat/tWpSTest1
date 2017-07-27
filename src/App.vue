a4c9fc3b5286c4fd8de963d7ac6a0ab41d13ccef
Виктор <viktortat@gmail.com>
2017-07-27 23:36:36 +0300

    119

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <h2>Главная</h2>
+
+            <app-quote quote="Это прекрасная цитата!"></app-quote>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+
+    export default {
+        components: {
+            appQuote: Quote
+        }
+    }
+</script>
+
+<style>
+    .row{
+        background-color: bisque;
+        padding: 10px;
+        border: 1px solid #c6c4cb;
+    }
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>{{quote}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['quote']
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: beige;
+        border: 1px solid #5e43cc;
+        box-shadow: 1px 2px 5px #888888;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+</style>
